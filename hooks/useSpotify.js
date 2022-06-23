65988fc1b02eaa5eded7f06e594b8e59b2f66c4e
rizwanfarooq07 <rizwanfarooqui07.com>
2022-06-23 11:25:48 +0530

    updated urit

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -5,6 +5,7 @@ import SpotifyWebApi from "spotify-web-api-node";
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
   clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  redirectUri: process.env.NEXTAUTH_URL,
 });
 
 const useSpotify = () => {
