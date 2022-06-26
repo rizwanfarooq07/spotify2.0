a65352b6b8b689d5799284150d047472ad33cb7b
rizwanfarooq07 <rizwanfarooqui07.com>
2022-06-26 15:19:32 +0530

    updated playlist

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 
 export const playlistIdState = atom({
   key: "playlistIdState",
-  default: "29HdtPi2frtXyHoouwZNxF",
+  default: "37i9dQZF1DWXRqgorJj26U",
 });
 
 export const playlistState = atom({
